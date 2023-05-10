00ed993988c22e4ca7de1094fa06fcbae8335a1e
Djordje <dd222im@student.lnu.se>
2023-05-10 15:31:03 +0200

    Need to figure out how to compare the metrics but beside that everything seems to be okay.

--- Functions.py
+++ Functions.py
@@ -20,14 +20,12 @@ scaler = StandardScaler()
 X_train_sample_scaled = scaler.fit_transform(X_train_sample)
 
 def one_vs_all(X_train, y_train, X_test, C, gamma):
-    classifiers = {}
     predictions = {}
     for number in range(10):
         yy = np.where(y_train == str(number), 1, 0)
         classifier = SVC(C=C, gamma=gamma, kernel='rbf')
         classifier.fit(X_train, yy)
-        classifiers[number] = classifier
-        predictions[number] = classifiers[number].predict(X_test)
+        predictions[number] = classifier.predict(X_test)
     return predictions
 
 one_vs_all(X_train_sample_scaled, y_train_sample, scaler.transform(X_test), 4.92, 0.001)
