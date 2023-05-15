ab66ce2e17e677de3eb0609b9321a922f4e9b8e6
Djordje Dimitrov <110050027+djolodjolo23@users.noreply.github.com>
2023-05-15 14:28:22 +0200

    Final commit a3

--- Functions.py
+++ Functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.svm import SVC
 
 
@@ -11,24 +12,3 @@ def one_vs_all(X_train, y_train, X_test, C, gamma):
         predictions[number] = classifier.predict(X_test)
     return predictions
 
-def get_nodes_at_some_depth(tree, depth):
-    current_depth_nodes = [0]
-    for _ in range (depth):
-        next_depth_nodes = []
-        for node in current_depth_nodes:
-            if tree.tree_.children_left[node] != -1:
-                next_depth_nodes.append(tree.tree_.children_left[node])
-            if tree.tree_.children_right[node] != -1:
-                next_depth_nodes.append(tree.tree_.children_right[node])
-        current_depth_nodes = next_depth_nodes
-    return current_depth_nodes
-
-def get_accuracy_at_some_depth(tree, depth, X, y):
-    nodes = get_nodes_at_some_depth(tree, depth)
-    X_selected = X[:, tree.tree_.feature[nodes]]
-    y_pred = tree.predict(X_selected)
-    X_selected_at_depth = X_selected[np.isin(tree.apply(X_selected), nodes)]
-    y_pred_at_depth = y_pred[np.isin(tree.apply(X_selected), nodes)]
-    y_true_at_depth = y[np.isin(tree.apply(X_selected), nodes)]
-    accuracy = np.mean(y_pred_at_depth == y_true_at_depth)
-    return accuracy
